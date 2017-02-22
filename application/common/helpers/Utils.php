c1e825c4f34ecbd35e0a436cf9642f089c8f23b1
Billy Clark <billy_clark@sil.org>
2017-02-22 16:05:12 -0500

    namespace was wrong.

--- application/common/helpers/Utils.php
+++ application/common/helpers/Utils.php
@@ -1,16 +1,13 @@
 <?php
 namespace common\helpers;
 
-use Ramsey\Uuid;
+
+use Ramsey\Uuid\Uuid;
 
 class Utils
 {
     const DT_FMT = 'Y-m-d H:i:s';
 
-    /**
-     * Get a Type 4 UUID
-     * @return string
-     */
     public static function genUuid()
     {
         return Uuid::uuid4()->toString();
