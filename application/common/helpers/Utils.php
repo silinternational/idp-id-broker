2119b5890bcad1d2eb5666fe349a9618d7b489cd
Billy Clark <billy_clark@sil.org>
2017-03-01 09:18:28 -0500

    UUID not needed anymore.

--- application/common/helpers/Utils.php
+++ application/common/helpers/Utils.php
@@ -1,15 +1,7 @@
 <?php
 namespace common\helpers;
 
-
-use Ramsey\Uuid\Uuid;
-
 class Utils
 {
     const DT_FMT = 'Y-m-d H:i:s';
-
-    public static function genUuid()
-    {
-        return Uuid::uuid4()->toString();
-    }
 }
