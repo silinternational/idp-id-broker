89bb125a0a2aebaf395a68b588814f2becbb12db
Billy Clark <billy_clark@sil.org>
2017-03-10 21:35:59 -0500

    thinking I may not need codeception at this time.

--- application/run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Run database migrations
-/data/yii migrate --interactive=0
-/data/yii migrate --interactive=0 --migrationPath=console/migrations-test
-
-# Run codeception tests
-cd /data
-codecept run unit
