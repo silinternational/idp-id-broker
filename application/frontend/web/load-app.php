9779ee0dff75bbdd17df59e4f515e50306c1334c
Billy Clark <billy_clark@sil.org>
2017-03-13 09:57:38 -0400

    didn't like the approach for setting up debugging.

--- application/frontend/web/load-app.php
+++ application/frontend/web/load-app.php
@@ -1,12 +1,11 @@
 <?php
 
-require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../vendor/autoload.php'); //TODO: why is this required for the web app but not the tests?
 
 define('YII_ENV', Sil\PhpEnv\Env::get('APP_ENV', 'prod'));
+define('YII_DEBUG', YII_ENV !== 'prod');
 
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
-
-//define('YII_DEBUG', true);
 
 require(__DIR__ . '/../../common/config/bootstrap.php');
 
