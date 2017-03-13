b5f54a9d6e2ced9e7c52983ef7b3ae94f8b66385
Billy Clark <billy_clark@sil.org>
2017-03-13 10:32:32 -0400

    not currently being used

--- application/console/config/main.php
+++ application/console/config/main.php
@@ -1,25 +1,9 @@
 <?php
-// TODO: what's all this for?
 return [
     'id' => 'app-console',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log', 'gii'],
-    'controllerNamespace' => 'console\controllers',
+    'bootstrap' => ['gii'],
     'modules' => [
         'gii' => 'yii\gii\Module',
-    ],
-    'components' => [
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                    'logVars' => [], // Disable logging of _SERVER, _POST, etc.
-                ],
-            ],
-        ],
-    ],
-    'params' => [
-
     ],
 ];
