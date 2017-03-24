7db7d5f076de08f2d2d7af0fb1f97dddd9426a36
Billy Clark <billy_clark@sil.org>
2017-03-24 11:24:23 -0400

    wanted to answer some "why?" questions that might come up for a new user to the code.

--- application/frontend/config/main.php
+++ application/frontend/config/main.php
@@ -3,42 +3,35 @@
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
+    // http://www.yiiframework.com/doc-2.0/guide-structure-applications.html#controllerNamespace
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
+        // http://www.yiiframework.com/doc-2.0/guide-security-authentication.html
         'user' => [
-            'identityClass' => 'common\models\ApiConsumer',
-            'enableAutoLogin' => false,
-            'enableSession' => false,
-            'loginUrl' => null,
+            'identityClass' => 'common\models\ApiConsumer', // custom Bearer <token> implementation
+            'enableSession' => false, // ensure statelessness
         ],
+        // http://www.yiiframework.com/doc-2.0/guide-runtime-requests.html
         'request' => [
+            // restrict input to JSON only http://www.yiiframework.com/doc-2.0/guide-rest-quick-start.html#enabling-json-input
             'parsers' => [
-                'application/json' => 'yii\web\JsonParser', // required according to http://www.yiiframework.com/doc-2.0/guide-rest-quick-start.html#enabling-json-input
+                'application/json' => 'yii\web\JsonParser',
             ]
         ],
+        // http://www.yiiframework.com/doc-2.0/guide-runtime-responses.html
         'response' => [
+            // all responses, even unhandled errors, need to be in JSON for an API.
             'format' => yii\web\Response::FORMAT_JSON,
         ],
+        // http://www.yiiframework.com/doc-2.0/guide-runtime-routing.html
         'urlManager' => [
-            'enablePrettyUrl' => true,
-            'enableStrictParsing' => true,
-            'showScriptName' => false,
+            'enablePrettyUrl' => true, // turns /index.php?r=post%2Fview&id=100 into /index.php/post/100
+            'showScriptName' => false, // turns /index.php/post/100 into /post/100
             'rules' => [
-                /*
-                 * Status
-                 */
-                'GET /site/system-status' => 'site/system-status',
-
-                /*
-                 * User
-                 */
-                'POST /user' => 'user/create',
-                'PUT /user/<employeeId:\w+>/password' => 'user/update-password',
-
-                /*
-                 * Authentication
-                 */
-                'POST /authentication' => 'authentication/create',
+                'POST /authentication'                 => 'authentication/create',
+                'POST /user'                           => 'user/create',
+                'PUT  /user/<employeeId:\w+>/password' => 'user/update-password',
+                'GET  /site/system-status'             => 'site/system-status',
             ]
         ],
     ],
