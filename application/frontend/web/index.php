58b685127b9821b785974b0af01968e975df9f28
Matt Henderson <matt_henderson@sil.org>
2017-05-18 12:01:07 -0400

    Log to syslog for env-var-missing errors in web requests.

--- application/frontend/web/index.php
+++ application/frontend/web/index.php
@@ -5,15 +5,20 @@ try {
      *       this required file.  */
     $config = require('../config/load-configs.php');
 } catch (Sil\PhpEnv\EnvVarNotFoundException $e) {
+    
+    // Log to syslog (Logentries).
+    openlog('id-broker', LOG_NDELAY | LOG_PERROR, LOG_USER);
+    syslog(LOG_CRIT, $e->getMessage());
+    closelog();
+    
+    // Return error response code/message to HTTP request.
     header('Content-Type: application/json');
     http_response_code(500);
-
     $responseContent = json_encode([
         'name' => 'Internal Server Error',
         'message' => $e->getMessage(),
         'status' => 500,
     ], JSON_PRETTY_PRINT);
-
     exit($responseContent);
 }
 
