d93ba1e4bd76b95859e8f92a9b5ea75dd9afb438
Matt Henderson <matt_henderson@sil.org>
2017-08-11 12:16:00 -0400

    Add several more email tests.

--- application/features/bootstrap/EmailContext.php
+++ application/features/bootstrap/EmailContext.php
@@ -34,16 +34,73 @@ class EmailContext implements Context
      */
     public function aEmailShouldHaveBeenSentToThem($messageType)
     {
+        $matchingFakeEmails = $this->getFakeEmailsOfTypeSentToUser(
+            $messageType,
+            $this->tempUser
+        );
+        Assert::greaterThan(count($matchingFakeEmails), 0);
+    }
+
+    /**
+     * @Then a(n) :messageType email should NOT have been sent to them
+     */
+    public function aEmailShouldNotHaveBeenSentToThem($messageType)
+    {
+        $matchingFakeEmails = $this->getFakeEmailsOfTypeSentToUser(
+            $messageType,
+            $this->tempUser
+        );
+        Assert::isEmpty($matchingFakeEmails);
+    }
+
+    /**
+     * @Then a(n) :messageType email to that user should NOT have been logged
+     */
+    public function aEmailToThatUserShouldNotHaveBeenLogged($messageType)
+    {
+        $emailLogs = EmailLog::findAll([
+            'message_type' => $messageType,
+            'user_id' => $this->tempUser->id,
+        ]);
+        Assert::isEmpty($emailLogs, sprintf(
+            'Expected NOT to find any email logs for a(n) %s email to User %s, '
+            . 'but instead found %s of them.',
+            var_export($messageType, true),
+            var_export($this->tempUser->id, true),
+            count($emailLogs)
+        ));
+        Assert::false(
+            $this->tempUser->hasReceivedMessage($messageType),
+            'User::hasReceivedMessage() unexpectedly returned true.'
+        );
+    }
+    
+    /**
+     * Get the actual email data (from our FakeEmailer) of any emails sent to
+     * the given user and of the specified type.
+     *
+     * @param string $messageType The type of message.
+     * @param User $user The User in question.
+     * @return array[]
+     */
+    protected function getFakeEmailsOfTypeSentToUser(
+        string $messageType,
+        User $user
+    ) {
         $fakeEmailer = $this->fakeEmailer;
-        $foundIt = false;
-        foreach ($fakeEmailer->getFakeEmailsSent() as $fakeEmail) {
-            $subject = $fakeEmail['subject'] ?? '';
-            if ($fakeEmailer->isSubjectForMessageType($subject, $messageType)) {
-                $foundIt = true;
-                break;
+        $fakeEmailsSent = $fakeEmailer->getFakeEmailsSent();
+        
+        return array_filter(
+            $fakeEmailsSent,
+            function ($fakeEmail) use ($fakeEmailer, $messageType, $user) {
+                
+                $subject = $fakeEmail['subject'] ?? '';
+                $toAddress = $fakeEmail['to_address'] ?? '';
+                
+                return $fakeEmailer->isSubjectForMessageType($subject, $messageType)
+                    && ($toAddress === $user->email);
             }
-        }
-        return $foundIt;
+        );
     }
     
     protected function giveYiiFakeEmailer()
@@ -112,5 +169,21 @@ class EmailContext implements Context
     public function weAreConfiguredToSendWelcomeEmails()
     {
         $this->fakeEmailer->sendWelcomeEmails = true;
+    }
+
+    /**
+     * @Given we are NOT configured to send invite emails
+     */
+    public function weAreNotConfiguredToSendInviteEmails()
+    {
+        $this->fakeEmailer->sendInviteEmails = false;
+    }
+
+    /**
+     * @Given we are NOT configured to send welcome emails
+     */
+    public function weAreNotConfiguredToSendWelcomeEmails()
+    {
+        $this->fakeEmailer->sendWelcomeEmails = false;
     }
 }
