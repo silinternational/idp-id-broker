91d3d2a959a5c8a08e377c224168af69ee14cecf
Matt Henderson <matt_henderson@sil.org>
2017-11-07 13:12:57 -0500

    Implement first MFA rate-limit feature test step.

--- application/features/bootstrap/MfaRateLimitContext.php
+++ application/features/bootstrap/MfaRateLimitContext.php
@@ -9,4 +9,67 @@ use Webmozart\Assert\Assert;
 
 class MfaRateLimitContext extends YiiContext
 {
+    /**
+     * Create a new user in the database with the given username (and other
+     * details based off that username). If a user already exists with that
+     * username, they will be deleted.
+     *
+     * @param string $username
+     * @return User
+     */
+    protected function createNewUserInDatabase($username)
+    {
+        $existingUser = User::findByUsername($username);
+        if ($existingUser !== null) {
+            Assert::notSame($existingUser->delete(), false);
+        }
+
+        $user = new User([
+            'email' => $username . '@example.com',
+            'employee_id' => (string)time(),
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'username' => $username,
+        ]);
+        $user->scenario = User::SCENARIO_NEW_USER;
+        Assert::true(
+            $user->save(),
+            var_export($user->getErrors(), true)
+        );
+        Assert::notNull($user);
+        return $user;
+    }
+
+    /**
+     * @Given I have a user with backup codes available
+     */
+    public function iHaveAUserWithBackupCodesAvailable()
+    {
+        $user = $this->createNewUserInDatabase('has_backupcodes_' . time());
+        $mfa = Mfa::create($user->id, Mfa::TYPE_BACKUPCODE);
+    }
+
+    /**
+     * @Given that MFA method has no recent failures
+     */
+    public function thatMfaMethodHasNoRecentFailures()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I submit a correct backup code
+     */
+    public function iSubmitACorrectBackupCode()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the backup code should be accepted
+     */
+    public function theBackupCodeShouldBeAccepted()
+    {
+        throw new PendingException();
+    }
 }
