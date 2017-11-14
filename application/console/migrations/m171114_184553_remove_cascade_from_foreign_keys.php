a857430954655c84c0f7273003ac64fd1e59110c
Matt Henderson <matt_henderson@sil.org>
2017-11-14 14:53:11 -0500

    Add migration to remove SQL-managed delete/update CASCADEs.

---
+++ application/console/migrations/m171114_184553_remove_cascade_from_foreign_keys.php
@@ -0,0 +1,68 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m171114_184553_remove_cascade_from_foreign_keys
+ */
+class m171114_184553_remove_cascade_from_foreign_keys extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        // Stop the User -> Current Password foreign key from deleting the User
+        // if/when the Password is deleted.
+        $this->dropForeignKey('fk_user_to_current_password', '{{user}}');
+        $this->addForeignKey(
+            'fk_user_to_current_password',
+            '{{user}}',
+            'current_password_id',
+            '{{password}}',
+            'id',
+            'NO ACTION',
+            'NO ACTION'
+        );
+        
+        // For consistency's sake, also remove any other CASCADE-ing foreign
+        // keys we've set up, using the models' beforeDelete() methods instead.
+        $this->dropForeignKey('fk_user_id', '{{email_log}}');
+        $this->addForeignKey(
+            'fk_user_id',
+            '{{email_log}}',
+            'user_id',
+            '{{user}}',
+            'id',
+            'NO ACTION',
+            'NO ACTION'
+        );
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_user_id', '{{email_log}}');
+        $this->addForeignKey(
+            'fk_user_id',
+            '{{email_log}}',
+            'user_id',
+            '{{user}}',
+            'id',
+            'CASCADE', // If that `user` is deleted, DELETE this `email_log` entry too.
+            'CASCADE' // If that `user.id` value changes, UPDATE this `email_log.user_id` too.
+        );
+        
+        $this->dropForeignKey('fk_user_to_current_password', '{{user}}');
+        $this->addForeignKey(
+            'fk_user_to_current_password',
+            '{{user}}',
+            'current_password_id',
+            '{{password}}',
+            'id',
+            'CASCADE' // This was the problem: deleting that password deletes this user.
+        );
+    }
+}
