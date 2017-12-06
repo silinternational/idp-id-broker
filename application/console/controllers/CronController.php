5603110816895d345b008d575c66eea943654001
Unknown <baggerone@gmail.com>
2017-12-06 14:59:50 -0500

    Created cron to report to Google Analytics about active users with verified mfas

--- application/console/controllers/CronController.php
+++ application/console/controllers/CronController.php
@@ -2,7 +2,11 @@
 namespace console\controllers;
 
 use common\models\Mfa;
+use common\models\User;
 use yii\console\Controller;
+use Sil\PhpEnv\Env;
+
+use TheIconic\Tracking\GoogleAnalytics\Analytics;
 
 class CronController extends Controller
 {
@@ -13,5 +17,52 @@ class CronController extends Controller
             'status' => 'starting',
         ]);
         Mfa::removeOldUnverifiedRecords();
+    }
+
+    public function actionGoogleAnalytics()
+    {
+        $trackingId = Env::get('GA_TRACKING_ID', null); // 'UA-12345678-12'
+        if ($trackingId === null) {
+            throw new \Exception('GA_TRACKING_ID is required');
+        }
+
+        $clientId = Env::get('GA_CLIENT_ID', null); // 'IDP_ID_BROKER_LOCALHOST'
+        if ($clientId === null) {
+            throw new \Exception('GA_CLIENT_ID is required');
+        }
+
+        $eventCategory = 'mfa-usage';
+
+        $gaEvents = [
+            'active-users' => User::find()->where(['active' => 'yes'])->count(),
+            'active-users-with-mfas' => User::getQueryOfUsersWithMfa()->count(),
+            'active-users-with-backup-codes' => User::getQueryOfUsersWithMfa(Mfa::TYPE_BACKUPCODE)->count(),
+            'active-users-with-totp' => User::getQueryOfUsersWithMfa(Mfa::TYPE_TOTP)->count(),
+            'active-users-with-u2f' => User::getQueryOfUsersWithMfa(Mfa::TYPE_U2F)->count(),
+        ];
+
+        $analytics = new Analytics();
+//  For debugging Google Analytics, uncomment next two lines and the Yii::warning block below and comment out the first $analytics->setProtocolVersion line
+//        $response = $analytics->setProtocolVersion('1')
+//            ->setDebug(true)
+        $analytics->setProtocolVersion('1')
+            ->setTrackingId($trackingId)
+            ->setClientId($clientId)
+            ->setEventCategory($eventCategory);
+
+
+        foreach ($gaEvents as $label => $value) {
+            $analytics->setEventLabel($label)
+                ->setEventValue($value)
+                ->sendEvent();
+        }
+
+//        \Yii::warning([
+//            'results' => $response->getDebugResponse(),
+//        ]);
+
+        $gaEvents['action'] = 'completed posting to Google Analytics';
+
+        \Yii::warning($gaEvents);
     }
 }
