c5bdc37f2a29984a0731e1be4b665cb27d97a3a4
Unknown <baggerone@gmail.com>
2017-12-11 14:09:16 -0500

    Added new email templates

---
+++ application/common/mail/get-backup-codes.html.php
@@ -0,0 +1,10 @@
+<?php
+use yii\helpers\Html as yHtml;
+
+/**
+ * @var string $displayName
+ */
+?>
+<p>Dear <?= yHtml::encode($displayName) ?>,</p>
+
+<h2>TODO: Compose contents of this email message</h2>
---
+++ application/common/mail/lost-security-key.text.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @var string $displayName
+ */
+?>
+Dear <?= $displayName ?>,
+
+TODO: Compose contents of this email message.
