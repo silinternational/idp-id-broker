68cfad52678d1af887415b559176667b98526105
Unknown <baggerone@gmail.com>
2017-12-13 09:13:48 -0500

    Add tests for new function in MySqlDateTime

---
+++ application/features/bootstrap/MySqlDateTimeContext.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Sil\SilIdBroker\Behat\Context;
+
+use Behat\Behat\Context\Context;
+use common\helpers\MySqlDateTime;
+use Webmozart\Assert\Assert;
+
+class MySqlDateTimeContext implements Context
+{
+
+
+    /** @var  bool */
+    protected $dateIsRecent;
+
+    /** @var  int */
+    protected $recentDays;
+
+    /**
+     * @Given I say that recent is in the last :arg1 days
+     */
+    public function iSayThatRecentIsInTheLastXDays($recentDays)
+    {
+        $this->recentDays = $recentDays;
+    }
+
+    /**
+     * @When  I ask if :arg1 days ago is recent
+     */
+    public function iAskIfXDaysAgoIsRecent($daysAgo)
+    {
+        $diffConfig = "-" . $daysAgo . " days";
+        $dbDate = MySqlDateTime::relative($diffConfig);
+        echo "dbdate: " . $dbDate;
+        $this->dateIsRecent = MySqlDateTime::dateIsRecent($dbDate, $this->recentDays);
+    }
+
+
+    /**
+     * @Then I see that that date is recent
+     */
+    public function iSeeThatThatDateIsRecent()
+    {
+        Assert::True($this->dateIsRecent);
+    }
+
+
+    /**
+     * @Then I see that that date is not recent
+     */
+    public function iSeeThatThatDateIsNotRecent()
+    {
+        Assert::False($this->dateIsRecent);
+    }
+}
---
+++ application/features/mysql-date-time.feature
@@ -0,0 +1,22 @@
+
+Feature: MysqlDateTime
+
+  Scenario: Check that a recent date is recognized as recent
+    Given I say that recent is in the last 3 days
+    When I ask if 3 days ago is recent
+    Then I see that that date is recent
+
+  Scenario: Check that today is recognized as recent
+    Given I say that recent is in the last 3 days
+    When I ask if 0 days ago is recent
+    Then I see that that date is recent
+
+  Scenario: Check that a barely old date is recognized as not recent
+    Given I say that recent is in the last 3 days
+    When I ask if 4 days ago is recent
+    Then I see that that date is not recent
+
+  Scenario: Check that an old date is recognized as not recent
+    Given I say that recent is in the last 3 days
+    When I ask if 24 days ago is recent
+    Then I see that that date is not recent
