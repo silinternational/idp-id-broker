04f96051696213fc9130467e0774beec07d1c9a6
Unknown <baggerone@gmail.com>
2017-12-19 16:35:34 -0500

    Added in variable section at top

--- application/common/mail/mfa-option-removed.text.php
+++ application/common/mail/mfa-option-removed.text.php
@@ -1,6 +1,26 @@
 <?php
+
 /**
+ * @var string $employeeId
+ * @var string $firstName
+ * @var string $lastName
  * @var string $displayName
+ * @var string $username
+ * @var string $email
+ * @var string $active
+ * @var string $locked
+ * @var string $lastChangedUtc
+ * @var string $lastSyncedUtc
+ * @var string $lastLoginUtc
+ * @var string $passwordExpiresUtc
+ * @var string $emailSignature
+ * @var string $helpCenterUrl
+ * @var string $idpDisplayName
+ * @var string $passwordForgotUrl
+ * @var string $passwordProfileUrl
+ * @var string $supportEmail
+ * @var string $supportName
+ * @var bool   $isMfaEnabled
  */
 ?>
 Dear <?= $displayName ?>,
