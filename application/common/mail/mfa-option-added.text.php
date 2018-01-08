24f199d8281a05ddafa71130035dfa1a3bac7ffc
Unknown <baggerone@gmail.com>
2018-01-08 16:32:42 -0500

    Added PHP_EOL on the end of lines in a list

--- application/common/mail/mfa-option-added.text.php
+++ application/common/mail/mfa-option-added.text.php
@@ -33,7 +33,7 @@ method of 2-Step Verification if you do not have access to the primary method.
 You now have the following 2-Step Verification options enabled:
 <?php
     foreach ($mfaOptions as $mfa) {
-       echo ' - ' . $mfa->getReadableType();
+       echo ' - ' . $mfa->getReadableType() . PHP_EOL;
     }
 ?>
 
--- application/common/mail/mfa-option-removed.text.php
+++ application/common/mail/mfa-option-removed.text.php
@@ -34,7 +34,7 @@ log in if needed, and add it back to your account.
 You can continue to use 2-Step Verification using the following option(s):
 <?php
 foreach ($mfaOptions as $mfa) {
-    echo ' - ' . $mfa->getReadableType();
+    echo ' - ' . $mfa->getReadableType() . PHP_EOL;
 }
 ?>
 
