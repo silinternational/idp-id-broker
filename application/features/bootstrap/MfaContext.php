092e98fb17f4790eab5efb9efc5c63beddbc371b
Steven Schram <steve_schram@sil.org>
2018-10-25 17:03:57 -0400

    move shared methods to parent class FeatureContext

--- application/features/bootstrap/MfaContext.php
+++ application/features/bootstrap/MfaContext.php
@@ -10,35 +10,6 @@ use Webmozart\Assert\Assert;
 
 class MfaContext extends \FeatureContext
 {
-    protected $tempEmployeeId = null;
-    
-    /**
-     * @Given I add a user with a(n) :property of :value
-     */
-    public function iAddAUserWithAnOf($property, $value)
-    {
-        $sampleUserData = [
-            'employee_id' => '10000',
-            'first_name' => 'John',
-            'last_name' => 'Smith',
-            'display_name' => 'John Smith',
-            'username' => 'john_smith',
-            'email' => 'john_smith@example.org',
-        ];
-        $sampleUserData[$property] = $value;
-        
-        $this->tempEmployeeId = $sampleUserData['employee_id'];
-        
-        $dataForTableNode = [
-            ['property', 'value'],
-        ];
-        foreach ($sampleUserData as $sampleProperty => $sampleValue) {
-            $dataForTableNode[] = [$sampleProperty, $sampleValue];
-        }
-        $this->iProvideTheFollowingValidData(new TableNode($dataForTableNode));
-        $this->iRequestTheResourceBe('/user', 'created');
-        $this->theResponseStatusCodeShouldBe(200);
-    }
 
     /**
      * @Given that user has a verified :mfaType MFA
@@ -57,13 +28,5 @@ class MfaContext extends \FeatureContext
         if ($mfaType === 'backupcode') {
             MfaBackupcode::createBackupCodes($mfa->id, 10);
         }
-    }
-
-    /**
-     * @Then I should receive :numRecords record(s)
-     */
-    public function iShouldReceiveRecords($numRecords)
-    {
-        $this->iShouldReceiveUsers($numRecords);
     }
 }
