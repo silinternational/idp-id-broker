ea812613e6e49ad1029b6205b854859f01b18c79
Steven Schram <steve_schram@sil.org>
2018-11-28 09:19:04 -0700

    Added to method verify email templates

--- application/common/mail/method-verify.html.php
+++ application/common/mail/method-verify.html.php
@@ -2,14 +2,18 @@
 use yii\helpers\Html as yHtml;
 
 /**
+ * @var string $displayName     Email address to be verified. Provided by user profile.
  * @var string $toAddress       Email address to be verified. Generated at runtime.
- * @var string $idpDisplayName  Display name of IDP instance. Default provided by environment variable IDP_DISPLAY_NAME.
+ * @var string $idpDisplayName  Display name of IDP instance. Provided by environment variable IDP_DISPLAY_NAME.
  * @var string $code            Verification code. Generated at runtime.
- * @var string $helpCenterUrl   Help center website URL.  Default provided by environment variable HELP_CENTER_URL.
- * @var string $emailSignature  Email signature. Default provided by environment variable EMAIL_SIGNATURE.
+ * @var string $supportName     Help center name.  Provided by environment variable SUPPORT_NAME.
+ * @var string $supportEmail    Help center email address.  Provided by environment variable SUPPORT_EMAIL.
+ * @var string $helpCenterUrl   Help center website URL.  Provided by environment variable HELP_CENTER_URL.
+ * @var string $emailSignature  Email signature. Provided by environment variable EMAIL_SIGNATURE.
  */
 ?>
-Hi there,
+<p>Dear <?= yHtml::encode($displayName) ?>,</p>
+
 <p>
     Someone recently requested to add this email address, <?= yHtml::encode($toAddress); ?>,
     as a method for verifying themselves should they need to reset their
@@ -20,7 +24,8 @@ Hi there,
     Verification Code: <?= yHtml::encode($code); ?>
 </p>
 <p>
-    If you did not request adding this email address to your account please delete this email.
+    If you did not do this it could be a sign someone else has compromised your account.
+    Please contact <?= yHtml::encode($supportName) ?> at <?= yHtml::encode($supportEmail) ?> as soon as possible to report the incident.
 </p>
 <p>
     To keep your account secure, please don't forward this email to anyone.
@@ -28,5 +33,7 @@ Hi there,
 </p>
 <p>
     Thanks!
-    - <?= yHtml::encode($emailSignature); ?>
+</p>
+<p>
+    <i><?= yHtml::encode($emailSignature); ?></i>
 </p>
--- application/common/mail/method-verify.text.php
+++ application/common/mail/method-verify.text.php
@@ -1,15 +1,17 @@
 <?php
-use yii\helpers\Html as yHtml;
 
 /**
+ * @var string $displayName     Email address to be verified. Provided by user profile.
  * @var string $toAddress       Email address to be verified. Generated at runtime.
- * @var string $idpDisplayName  Display name of IDP instance. Default provided by environment variable IDP_DISPLAY_NAME.
+ * @var string $idpDisplayName  Display name of IDP instance. Provided by environment variable IDP_DISPLAY_NAME.
  * @var string $code            Verification code. Generated at runtime.
- * @var string $helpCenterUrl   Help center website URL.  Default provided by environment variable HELP_CENTER_URL.
- * @var string $emailSignature  Email signature. Default provided by environment variable EMAIL_SIGNATURE.
+ * @var string $supportName     Help center name.  Provided by environment variable SUPPORT_NAME.
+ * @var string $supportEmail    Help center email address.  Provided by environment variable SUPPORT_EMAIL.
+ * @var string $helpCenterUrl   Help center website URL.  Provided by environment variable HELP_CENTER_URL.
+ * @var string $emailSignature  Email signature. Provided by environment variable EMAIL_SIGNATURE.
  */
 ?>
-Hi there,
+    Dear <?= $displayName ?>,
 
     Someone recently requested to add this email address, <?= $toAddress ?>,
     as a method for verifying themselves should they need to reset their
@@ -18,11 +20,13 @@ Hi there,
 
     Verification Code: <?= $code ?>
 
-    If you did not request adding this email address to your account please delete this email.
+    If you did not do this it could be a sign someone else has compromised
+    your account. Please contact <?= $supportName ?> at <?= $supportEmail ?>
+    as soon as possible to report the incident.
 
     To keep your account secure, please don't forward this email to anyone.
-    See our Help Center for <a href="<?= $helpCenterUrl ?>">more security tips</a>.
+    See our Help Center at <?= $helpCenterUrl ?> for more security tips.
 
     Thanks!
-    - <?= $emailSignature ?>
+    <?= $emailSignature ?>
 
