af830f9a05bbb683a0e3113bd006a7d5c11435d8
Steven Schram <steve_schram@sil.org>
2018-12-13 15:20:15 -0700

    add method verify url to email template

--- application/common/mail/method-verify.html.php
+++ application/common/mail/method-verify.html.php
@@ -5,11 +5,13 @@ use yii\helpers\Html as yHtml;
  * @var string $displayName     Email address to be verified. Provided by user profile.
  * @var string $toAddress       Email address to be verified. Generated at runtime.
  * @var string $idpDisplayName  Display name of IDP instance. Provided by environment variable IDP_DISPLAY_NAME.
+ * @var string $uid             Method uid. Generated at runtime.
  * @var string $code            Verification code. Generated at runtime.
  * @var string $supportName     Help center name.  Provided by environment variable SUPPORT_NAME.
  * @var string $supportEmail    Help center email address.  Provided by environment variable SUPPORT_EMAIL.
  * @var string $helpCenterUrl   Help center website URL.  Provided by environment variable HELP_CENTER_URL.
  * @var string $emailSignature  Email signature. Provided by environment variable EMAIL_SIGNATURE.
+ * @var string $passwordProfileUrl  URL of password manager. Provided by environment variable PASSWORD_PROFILE_URL.
  */
 ?>
 <p>Dear <?= yHtml::encode($displayName) ?>,</p>
@@ -17,8 +19,9 @@ use yii\helpers\Html as yHtml;
 <p>
     Someone recently requested to add this email address, <?= yHtml::encode($toAddress); ?>,
     as a method for verifying themselves should they need to reset their
-    <?= yHtml::encode($idpDisplayName); ?> account password. If this was you, you can use the verification code
-    below to add it to your account.
+    <?= yHtml::encode($idpDisplayName); ?> account password. If this was you, you may click
+    <?= yHtml::a('here', $passwordProfileUrl . '/password/recovery/' . $uid . '/verify?code=' . $code) ?>
+    to add it to your account.
 </p>
 <p>
     Verification Code: <?= yHtml::encode($code); ?>
--- application/common/mail/method-verify.text.php
+++ application/common/mail/method-verify.text.php
@@ -4,19 +4,22 @@
  * @var string $displayName     Email address to be verified. Provided by user profile.
  * @var string $toAddress       Email address to be verified. Generated at runtime.
  * @var string $idpDisplayName  Display name of IDP instance. Provided by environment variable IDP_DISPLAY_NAME.
+ * @var string $uid             Method uid. Generated at runtime.
  * @var string $code            Verification code. Generated at runtime.
  * @var string $supportName     Help center name.  Provided by environment variable SUPPORT_NAME.
  * @var string $supportEmail    Help center email address.  Provided by environment variable SUPPORT_EMAIL.
  * @var string $helpCenterUrl   Help center website URL.  Provided by environment variable HELP_CENTER_URL.
  * @var string $emailSignature  Email signature. Provided by environment variable EMAIL_SIGNATURE.
+ * @var string $passwordProfileUrl  URL of password manager. Provided by environment variable PASSWORD_PROFILE_URL.
  */
 ?>
     Dear <?= $displayName ?>,
 
     Someone recently requested to add this email address, <?= $toAddress ?>,
     as a method for verifying themselves should they need to reset their
-    <?= $idpDisplayName ?> account password. If this was you, you can use the verification code
-    below to add it to your account.
+    <?= $idpDisplayName ?> account password. If this was you, you may go to
+    $passwordProfileUrl . '/password/recovery/' . $uid . '/verify?code=' . $code
+    to add it to your account.
 
     Verification Code: <?= $code ?>
 
