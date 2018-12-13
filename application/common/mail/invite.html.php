161a80b38c9c6317f879215d4a8be198ae5b1d3f
Steven Schram <steve_schram@sil.org>
2018-12-13 07:49:14 -0700

    Remove whitespace inside anchor element

--- application/common/mail/invite.html.php
+++ application/common/mail/invite.html.php
@@ -47,9 +47,7 @@ use yii\helpers\Html as yHtml;
 <p><b>Instructions:</b></p>
 <p>
     To proceed with establishing your account, please follow the step-by-step guidance
-    <a href="<?= yHtml::encode($passwordProfileUrl . '/intro?invite=' . $inviteCode) ?>">
-        here
-    </a>.
+    <a href="<?= yHtml::encode($passwordProfileUrl . '/intro?invite=' . $inviteCode) ?>">here</a>.
 </p>
 
 <p>
