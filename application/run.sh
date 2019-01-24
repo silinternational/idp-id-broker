5bce4c23a899b38e10720330f49c2ad6b3e0994a
Steven Schram <steve_schram@sil.org>
2019-01-24 14:12:56 -0700

    trap SIGTERM to terminate apache cleanly

--- application/run.sh
+++ application/run.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+set -x
+
+# establish a signal handler to catch the SIGTERM from a 'docker stop'
+# reference: https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86
+term_handler() {
+  apache2ctl stop
+  exit 143; # 128 + 15 -- SIGTERM
+}
+trap 'kill ${!}; term_handler' SIGTERM
+
+touch /data/vendor/simplesamlphp/simplesamlphp/modules/exampleauth/enable
 
 if [[ "x" == "x$LOGENTRIES_KEY" ]]; then
     echo "Missing LOGENTRIES_KEY environment variable";
@@ -7,7 +18,7 @@ else
     sed -i /etc/rsyslog.conf -e "s/LOGENTRIESKEY/${LOGENTRIES_KEY}/"
     # Start syslog
     rsyslogd
-    
+
     # Give syslog time to fully start up.
     sleep 10
 fi
@@ -17,5 +28,11 @@ if [[ $APP_ENV == "dev" ]]; then
     apt-get -y -q install php-xdebug
 fi
 
-# Run apache in foreground
-apache2ctl -D FOREGROUND
+apache2ctl start
+
+# endless loop with a wait is needed for the trap to work
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
+
