e3574feca3c9e9505083f7feaabb42d05256873c
Schparky <3172830+Schparky@users.noreply.github.com>
2019-03-07 10:31:33 -0700

    push review dates for new users into the future

--- application/common/models/User.php
+++ application/common/models/User.php
@@ -171,10 +171,14 @@ class User extends UserBase
                 'require_mfa', 'default', 'value' => 'no', 'on' => self::SCENARIO_NEW_USER
             ],
             [
-                'nag_for_mfa_after', 'default', 'value' => MySqlDateTime::today(),
+                'nag_for_mfa_after',
+                'default',
+                'value' => MySqlDateTime::relative(\Yii::$app->params['mfaAddInterval']),
             ],
             [
-                'nag_for_method_after', 'default', 'value' => MySqlDateTime::today(),
+                'nag_for_method_after',
+                'default',
+                'value' => MySqlDateTime::relative(\Yii::$app->params['methodAddInterval']),
             ],
             [
                 ['active', 'locked', 'require_mfa', 'hide'], 'in', 'range' => ['yes', 'no'],
