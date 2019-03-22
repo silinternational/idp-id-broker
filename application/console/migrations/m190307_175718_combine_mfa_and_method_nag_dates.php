984de36ec2df82fb5fd05bf9a15ae78f1a051041
Schparky <3172830+Schparky@users.noreply.github.com>
2019-03-22 09:44:53 -0600

    reuse nag_for_mfa_after as review_profile_after to retain those dates

--- application/console/migrations/m190307_175718_combine_mfa_and_method_nag_dates.php
+++ application/console/migrations/m190307_175718_combine_mfa_and_method_nag_dates.php
@@ -13,8 +13,7 @@ class m190307_175718_combine_mfa_and_method_nag_dates extends Migration
     public function safeUp()
     {
         $this->dropColumn('{{user}}', 'nag_for_method_after');
-        $this->dropColumn('{{user}}', 'nag_for_mfa_after');
-        $this->addColumn('{{user}}', 'review_profile_after', 'date not null');
+        $this->renameColumn('{{user}}', 'nag_for_mfa_after', 'review_profile_after');
     }
 
     /**
@@ -22,8 +21,7 @@ class m190307_175718_combine_mfa_and_method_nag_dates extends Migration
      */
     public function safeDown()
     {
-        $this->dropColumn('{{user}}', 'review_profile_after');
-        $this->addColumn('{{user}}', 'nag_for_mfa_after', 'date not null');
+        $this->renameColumn('{{user}}', 'review_profile_after', 'nag_for_mfa_after');
         $this->addColumn('{{user}}', 'nag_for_method_after', 'date not null');
     }
 }
