0250d04e1be0ecb54edcbb4a2043ba1efdfb3bd7
Phillip Shipley <phillip.shipley@gmail.com>
2021-12-03 16:18:57 -0500

    Update application/frontend/controllers/MfaController.php
    Co-authored-by: forevermatt <forevermatt@users.noreply.github.com>

--- application/frontend/controllers/MfaController.php
+++ application/frontend/controllers/MfaController.php
@@ -122,7 +122,7 @@ class MfaController extends BaseRestController
 
         // rpOrigin is needed for WebAuthn authentication
         $rpOrigin = \Yii::$app->request->get('rporigin', '');
-        if ($rpOrigin != '' && !in_array($rpOrigin, \Yii::$app->params['authorizedRPOrigins'])){
+        if ($rpOrigin != '' && !in_array($rpOrigin, \Yii::$app->params['authorizedRPOrigins'], true)){
             throw new ForbiddenHttpException("Invalid rporigin", 1638378156);
         }
 
