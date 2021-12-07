5b76309e854079872c6db50ca489d049cecb350b
Phillip Shipley <phillip_shipley@sil.org>
2021-12-07 09:03:48 -0500

    start of hr notification email

---
+++ application/common/mail/invite-hr-version.html.php
@@ -0,0 +1,46 @@
+<?php
+use yii\helpers\Html as yHtml;
+
+/**
+ * @var string $firstName
+ * @var string $displayName
+ * @var string $username
+ * @var string $email
+ * @var string $passwordExpiresUtc
+ * @var string $emailSignature
+ * @var string $helpCenterUrl
+ * @var string $idpDisplayName
+ * @var string $passwordProfileUrl
+ * @var string $supportEmail
+ * @var string $supportName
+ * @var bool   $isMfaEnabled
+ */
+
+?>
+<p>Dear <?= yHtml::encode($displayName) ?>,</p>
+
+<p>
+    A new <?= yHtml::encode($idpDisplayName) ?> Identity account has been created for you and is ready for you to set
+    your password. Please note this account is not an email account. Instead this account is for use with
+    corporate applications.
+</p>
+
+<p>
+    If you have any difficulties completing this task, please contact
+    <?= yHtml::encode($supportName) ?> at: <?= yHtml::encode($supportEmail) ?>
+</p>
+
+<p><b>Instructions:</b></p>
+<p>
+    To set up your account, please follow the step-by-step guidance
+    here: {only sent to individual}
+</p>
+
+<p>
+    Please note that this <?= yHtml::encode($idpDisplayName) ?> Identity account (username and password) is
+    unique and its password is not synchronized with any other accounts you may have.
+</p>
+
+<p>Thank you,</p>
+
+<p><i><?= nl2br(yHtml::encode($emailSignature), false) ?></i></p>
