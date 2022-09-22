99a8e95500586992adc09bb089999ecb025161d7
stevebagwell <steve_bagwell@sil.org>
2022-09-22 13:44:49 +0100

    Don't delay startup for codeship

--- application/run-tests.sh
+++ application/run-tests.sh
@@ -7,22 +7,25 @@ composer install --no-interaction --no-scripts --no-progress
 # If that failed, exit.
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-# avoid having issues here due to the random sleep on the app/appfortests container
-i=10
+# avoid having issues locally due to the random sleep on the appfortests container
+i=0
+testServer=${TEST_SERVER_HOSTNAME}
+localServer='appfortests'
+
+if [[ "$testServer" == "$localServer" ]]; then
+  i=10
+fi
+
 while [ "$i" -ne 0 ]
 do
   want='Page not found.'
-  cRes="$(curl appfortests)"
+  cRes="$(curl $localServer)"
   if [[ "$cRes" == *"$want"* ]]; then
+    echo "Successfully connected to $localServer"
     break
   fi
 
-  # This applies to codeship tests
-  cRes="$(curl localhost)"
-  if [[ "$cRes" == *"$want"* ]]; then
-    break
-  fi
-  echo "Waiting for appfortests container " $i
+  echo "Waiting for $localServer container:" $i
   sleep 1
   ((i=i-1))
 done
