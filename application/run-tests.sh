d48baaea7a89e07d036b91e1d6abdfc55950dfe3
stevebagwell <steve_bagwell@sil.org>
2022-09-22 11:50:53 +0100

    avoid having issues due to the random sleep on the appfortests container

--- application/run-tests.sh
+++ application/run-tests.sh
@@ -7,6 +7,20 @@ composer install --no-interaction --no-scripts --no-progress
 # If that failed, exit.
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
+# avoid having issues here due to the random sleep on the appfortests container
+i=10
+while [ "$i" -ne 0 ]
+do
+  want='Page not found.'
+  cRes="$(curl appfortests)"
+  if [[ "$cRes" == *"$want"* ]]; then
+    break
+  fi
+  echo "Waiting for appfortests container " $i
+  sleep 1
+  ((i=i-1))
+done
+
 # Try to run database migrations
 whenavail testdb 3306 100 ./yii migrate --interactive=0
 
