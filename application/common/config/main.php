2d3a5a3db122c895c8d76f08c7ee862ce243ee7c
briskt <3172830+briskt@users.noreply.github.com>
2023-10-06 15:25:00 -0600

    appEnv isn't defined, use YII_ENV instead

--- application/common/config/main.php
+++ application/common/config/main.php
@@ -201,7 +201,7 @@ return [
                     // https://docs.sentry.io/platforms/php/configuration/options
                     'clientOptions' => [
                         'attach_stacktrace' => false, // stack trace identifies the logger call stack, not helpful
-                        'environment' => $appEnv,
+                        'environment' => YII_ENV,
                         'release' => 'idp-id-broker@6.6.0-pre',
                         'before_send' => function (Event $event) use ($idpName): ?Event {
                             $event->setExtra(['idp' => $idpName]);
