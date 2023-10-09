cf65b70adc37d6be080da0967e0c14e1ca3dd115
briskt <3172830+briskt@users.noreply.github.com>
2023-10-09 13:22:42 -0600

    update version string to 6.6.0

--- application/common/config/main.php
+++ application/common/config/main.php
@@ -202,7 +202,7 @@ return [
                     'clientOptions' => [
                         'attach_stacktrace' => false, // stack trace identifies the logger call stack, not helpful
                         'environment' => YII_ENV,
-                        'release' => 'idp-id-broker@6.6.0-pre',
+                        'release' => 'idp-id-broker@6.6.0',
                         'before_send' => function (Event $event) use ($idpName): ?Event {
                             $event->setExtra(['idp' => $idpName]);
                             return $event;
