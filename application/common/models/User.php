4ac97dedff59e4a8675a81a3531136c619e34579
briskt <3172830+briskt@users.noreply.github.com>
2023-11-08 10:44:07 -0700

    Revert "add temporary code to benchmark password hash cost"
    This reverts commit 4435d8ba6eef4eabe35ad91fbd00efe24ab92512.

--- application/common/models/User.php
+++ application/common/models/User.php
@@ -298,25 +298,6 @@ class User extends UserBase
             $currentPassword = $this->currentPassword ?? new Password();
             if (! password_verify($this->password, $currentPassword->hash)) {
                 $this->addError($attributeName, 'Incorrect password.');
-            } else {
-                /**
-                 * This code will benchmark your server to determine how high of a cost you can
-                 * afford. You want to set the highest cost that you can without slowing down
-                 * you server too much. 10 is a good baseline, and more is good if your servers
-                 * are fast enough. The code below aims for ≤ 350 milliseconds stretching time,
-                 * which is an appropriate delay for systems handling interactive logins.
-                 */
-                $timeTarget = 0.350; // 350 milliseconds
-
-                $cost = 10;
-                do {
-                    $cost++;
-                    $start = microtime(true);
-                    password_hash("test", PASSWORD_BCRYPT, ["cost" => $cost]);
-                    $end = microtime(true);
-                } while (($end - $start) < $timeTarget);
-
-                Yii::info("Optimal hash cost: " . $cost, 'application');
             }
         };
     }
