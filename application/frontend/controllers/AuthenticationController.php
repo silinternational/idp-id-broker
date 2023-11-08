789475f11cc17ab9985f9fe90f9f0492d22a6a6c
briskt <3172830+briskt@users.noreply.github.com>
2023-11-08 10:44:13 -0700

    Revert "put the password hash test at a higher level since it didn't work"
    This reverts commit 4213c525fc90d4a402a87d7401c0921466c5f885.

--- application/frontend/controllers/AuthenticationController.php
+++ application/frontend/controllers/AuthenticationController.php
@@ -37,29 +37,11 @@ class AuthenticationController extends BaseRestController
 
         $authenticatedUser = $authentication->getAuthenticatedUser();
 
-        /**
-         * This code will benchmark your server to determine how high of a cost you can
-         * afford. You want to set the highest cost that you can without slowing down
-         * you server too much. 10 is a good baseline, and more is good if your servers
-         * are fast enough. The code below aims for ≤ 350 milliseconds stretching time,
-         * which is an appropriate delay for systems handling interactive logins.
-         */
-        $timeTarget = 0.350; // 350 milliseconds
-
-        $cost = 10;
-        do {
-            $cost++;
-            $start = microtime(true);
-            password_hash("test", PASSWORD_BCRYPT, ["cost" => $cost]);
-            $end = microtime(true);
-        } while (($end - $start) < $timeTarget);
-
         $log = [
             'action' => 'authentication/create',
             'username' => $username,
             'password' => empty($password) ? 'no' : 'yes',
             'invite' => empty($inviteCode) ? 'no' : 'yes',
-            'cost' => $cost,
         ];
 
         if ($authenticatedUser !== null) {
