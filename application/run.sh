6601b6be5617d408a4101b42b916a55fa7587bcb
briskt <3172830+briskt@users.noreply.github.com>
2024-02-27 16:13:08 -0700

    remove useless signal trap
    -- it was broken by "-D FOREGROUND" which was required to send logs to CloudWatch

--- application/run.sh
+++ application/run.sh
@@ -6,14 +6,6 @@ set -x
 # exit if any line in the script fails
 set -e
 
-# establish a signal handler to catch the SIGTERM from a 'docker stop'
-# reference: https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86
-term_handler() {
-  apache2ctl stop
-  exit 143; # 128 + 15 -- SIGTERM
-}
-trap 'kill ${!}; term_handler' SIGTERM
-
 if [[ $APP_ENV == "dev" ]]; then
     export XDEBUG_CONFIG="remote_enable=1 remote_host="$REMOTE_DEBUG_IP
     apt-get -y -q install php-xdebug
@@ -36,10 +28,3 @@ if [[ ! -z $RUN_TASK ]]; then
 fi
 
 config-shim -v --app $APP_ID --config $CONFIG_ID --env $ENV_ID apache2ctl -k start -D FOREGROUND
-
-# endless loop with a wait is needed for the trap to work
-while true
-do
-  tail -f /dev/null & wait ${!}
-done
-
