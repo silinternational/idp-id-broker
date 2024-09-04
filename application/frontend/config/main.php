799cc614a15d1db416f49a3efefc93fe5627da54
Matt H <matt_henderson@sil.org>
2024-09-04 11:11:18 -0400

    Remove unneeded extra indentation

--- application/frontend/config/main.php
+++ application/frontend/config/main.php
@@ -45,12 +45,12 @@ return [
                 /*
                  * User routes
                  */
-                'GET  user'                                             => 'user/index',
-                'GET  user/<employeeId:\w+>'                            => 'user/view',
-                'POST user'                                             => 'user/create',
-                'PUT  user/<employeeId:\w+>'                            => 'user/update',
-                'PUT  user/<employeeId:\w+>/password'                   => 'user/update-password',
-                'PUT  user/<employeeId:\w+>/password/assess'            => 'user/assess-password',
+                'GET  user'                                  => 'user/index',
+                'GET  user/<employeeId:\w+>'                 => 'user/view',
+                'POST user'                                  => 'user/create',
+                'PUT  user/<employeeId:\w+>'                 => 'user/update',
+                'PUT  user/<employeeId:\w+>/password'        => 'user/update-password',
+                'PUT  user/<employeeId:\w+>/password/assess' => 'user/assess-password',
 
                 /*
                  * Authentication routes
